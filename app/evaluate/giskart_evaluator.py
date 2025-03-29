e0d09bda5bde96771302d07c78ab7443efac8d5d
leonbartz <leon.bartz@real-digital.de>
2025-03-29 18:40:55 +0100

    Enhance GiskartEvaluator by integrating new metrics for evaluation and updating the global LLM model management. Replace LiteLLMClient with OpenAIClient for knowledge base initialization and ensure proper model reset after report generation.

--- app/evaluate/giskart_evaluator.py
+++ app/evaluate/giskart_evaluator.py
@@ -1,14 +1,19 @@
 import os
+
 import openai
 from giskard.llm.client.openai import OpenAIClient
 from giskard.rag import evaluate as giskart_evaluate
+from giskard.llm import set_embedding_model
+from giskard.rag.metrics import CorrectnessMetric, correctness_metric
+from giskard.rag.metrics.ragas_metrics import ragas_context_precision, ragas_faithfulness, ragas_answer_relevancy, \
+    ragas_context_recall
 
 from app.chat_models import config
 from app.evaluate.evaluator import Evaluator
 import time
-from giskard.llm.client.litellm import LiteLLMClient
 from giskard.rag import KnowledgeBase
 import pandas as pd
+from app.chat_models.factory import ChatModelFactory
 
 
 class GiskartEvaluator(Evaluator):
@@ -16,9 +21,12 @@ class GiskartEvaluator(Evaluator):
 
     def generate_report(self):
         df = pd.DataFrame([d.page_content for d in self.documents], columns=["text"])
+
+        ChatModelFactory.set_global_llm_model("ollama/nomic-embed-text")
+
         knowledge_base = KnowledgeBase(
             df,
-            llm_client=LiteLLMClient(model=self.MODEL)
+            llm_client=OpenAIClient(self.MODEL, openai.OpenAI(api_key=config.OPENAI_API_KEY)),
         )
 
         def answer_fn(question, history=None):
@@ -26,13 +34,21 @@ class GiskartEvaluator(Evaluator):
 
         report = giskart_evaluate(
             answer_fn,
-            llm_client=OpenAIClient(self.MODEL, openai.OpenAI(api_key=config.OPENAI_API_KEY)),
             testset=self.testset,
-            knowledge_base=knowledge_base
+            knowledge_base=knowledge_base,
+            metrics=[
+                correctness_metric,
+                ragas_context_precision,
+                ragas_faithfulness,
+                ragas_answer_relevancy,
+                ragas_context_recall,
+            ],
         )
 
         filename = self.generate_filename()
         report.save(filename)
+
+        ChatModelFactory.reset_global_llm_model()
 
         return filename, report.component_scores(), report.knowledge_base_score
 
