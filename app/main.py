cea270bfd6365ed9884cee34697d0dd9a16e7d3e
leonbartz <leon.bartz@real-digital.de>
2025-03-29 14:35:56 +0100

    add more endpoints

--- app/main.py
+++ app/main.py
@@ -1,18 +1,30 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.chat_models.factory import ChatModelFactory
 from app.db import get_db
 from app.dto.create_documents_dto import CreateDocumentsDTO
 from app.dto.create_testset_dto import CreateTestsetDTO
 from app.models import RatingResult, Document, Testset
 from fastapi import FastAPI, APIRouter, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from app import db
 from app.dto.process_request_dto import ProcessRequestDTO
+from app.testset_loader.qa_testset_loader import QATestsetLoader
 from app.websocket import websocket_endpoint
 from app.tasks import process_chat_request, create_documents_request, create_testset_request
 from dotenv import load_dotenv
 
 load_dotenv()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3333"],  # Adjust this to your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
@@ -77,6 +89,19 @@ async def get_all_test_sets(db: AsyncSession = Depends(get_db)):
         print(f"❌ Error fetching test sets: {e}")
         return []
 
+@app.get("/testsets/{testset_id}")
+async def get_test_set(testset_id: int, db: AsyncSession = Depends(get_db)):
+    try:
+        result = await db.execute(select(Testset).filter(Testset.id == testset_id))
+        test_set = result.scalars().first()
+        test_set_loader = QATestsetLoader(f"app/data/testsets/{test_set.id}.jsonl")
+        test_set_content = test_set_loader.load()
+        test_set.questions = test_set_content.to_pandas().to_json()
+        return test_set
+    except Exception as e:
+        print(f"❌ Error fetching test set: {e}")
+        return []
+
 @app.post("/testsets/")
 async def create_test_set(testset_dto: CreateTestsetDTO, db: AsyncSession = Depends(get_db)):
     try:
@@ -99,6 +124,9 @@ async def create_test_set(testset_dto: CreateTestsetDTO, db: AsyncSession = Depe
         raise e
         return {"error": str(e)}
 
+@app.get('/models/')
+async def get_models():
+    return ChatModelFactory.available_models()
 
 router = APIRouter()
 
