69a64602566da3fd54457e91bb3959f994840b67
leonbartz <leon.bartz@real-digital.de>
2025-05-30 15:14:23 +0200

    - Added `GiskardOpenAIEmbeddingsCreator` to model factory and updated embedding creators with OpenAI support. - Changed default base URL in `OllamaEmbeddingsCreator` to `192.168.5.2:11434`.

--- app/embeddings/langchain/creator/openai_creator.py
+++ app/embeddings/langchain/creator/openai_creator.py
@@ -1,5 +1,7 @@
 from langchain_openai import OpenAIEmbeddings
 from app.embeddings.langchain.creator.base import LangchainEmbeddingsCreator
+from app.config.llm_config import OPENAI_API_KEY
+from pydantic import SecretStr
 
 class OpenAIEmbeddingsCreator(LangchainEmbeddingsCreator):
     def __init__(self, dimensions: int = 3072, model: str = "text-embedding-3-large"):
@@ -9,5 +11,6 @@ class OpenAIEmbeddingsCreator(LangchainEmbeddingsCreator):
     def create_embeddings(self) -> OpenAIEmbeddings:
         return OpenAIEmbeddings(
             dimensions=self.dimensions,
-            model=self.model
+            model=self.model,
+            api_key=SecretStr(OPENAI_API_KEY) if OPENAI_API_KEY else None
         )
